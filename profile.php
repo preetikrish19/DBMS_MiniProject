f9f25ebd3a686dc40f5890e7703626f880484f8f
hemagunnala <gunnalahema14134@gmail.com>
2021-05-29 10:17:59 +0530

    profile pages

--- profile.php
+++ profile.php
@@ -4,7 +4,33 @@
 
   <div class="post-form">
   <input type="text" placeholder="Update Name" name="name"><br />
-    <label for="cars">Choose the domain</label>
+ 
+  <input type="hidden" name="domain" value="0">
+  <label for="cars">Choose the domain</label>
+          <input type="checkbox" name="domain" value="1" class="">
+          <label for="DSA">DSA</label><br>
+
+          <input type="hidden" name="choice2" value="0">
+          <input type="checkbox" name="choice2" value="2" class="">
+          <label for="c">C programming</label><br>
+
+          <input type="hidden" name="choice3" value="0">
+          <input type="checkbox" name="choice3" value="3" class="">
+          <label for="dbms">DBMS</label><br>
+
+          <input type="hidden" name="choice4" value="0">
+          <input type="checkbox" name="choice4" value="4" class="">
+          <label for="os">OS</label><br>
+
+          <input type="hidden" name="choice5" value="0">
+          <input type="checkbox" name="choice5" value="5" class="">
+          <label for="os"></label>C++<br>
+
+          <input type="hidden" name="choice6" value="0">
+          <input type="checkbox" name="choice6" value="6" class="">
+          <label for="os"></label>Python<br> 
+    
+    <!--<label for="cars">Choose the domain</label>
     <select id="domains" name="Domains">
         <option value="2">C</option>
         <option value="5">C++</option>
@@ -12,12 +38,29 @@
         <option value="4">OS</option>
         <option value="3">DBMS</option>
         <option value="1">DSA</option>
-    </select>
+    </select>-->
     <br> 
     <textarea rows="5" cols="80" placeholder="Update Acheivements" name="description" ></textarea><br/>
     <input type="submit" class="button" value="Update"/><br/>
+
+   
     <!--<input type="submit" class="button" value="followers"/><br/> -->
     </div>
 </form>
+<br/>
+<?php
+echo "POSTS","<br>";
+session_start();
+include('db.php');
+$mentor_id = $_SESSION['mid'];
+$sql1 = "SELECT * FROM mentorpost WHERE mentor_id ='$mentor_id';";
+ $result = mysqli_query($con,$sql1);
+ $resultcheck = mysqli_num_rows($result);
+ if($resultcheck > 0){
+   while($row = mysqli_fetch_assoc($result)){
+        echo $row['file_name'], "<br>";
+   }
+  }
+   ?>
 
 
--- updateform.php
+++ updateform.php
@@ -7,18 +7,15 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
   $domains = $con->real_escape_string($_POST['Domains']); 
   $name= $con->real_escape_string($_POST['name']); 
   $desc= $con->real_escape_string($_POST['description']);
-/* $sql = "UPDATE  mentordetails SET domain = '$domains' where mid = '$mentor_id' ";
+  $choice2 = $con->real_escape_string($_POST['choice2']);
+  $choice3 = $con->real_escape_string($_POST['choice3']);
+  $choice4 = $con->real_escape_string($_POST['choice4']);
+  $choice5 = $con->real_escape_string($_POST['choice5']);
+  $choice6 = $con->real_escape_string($_POST['choice6']);
 
-  if(mysqli_query($con, $sql))
-  {
-    echo "posted";
-    echo "<script>window.location.href='index.php'</script>";
-  }
-  else{
-    echo "Error: " . $sql . "<br>" . $con->error;
-  } */
+  echo "$choice2";
 
-$sql = "UPDATE  mentordetails SET domain = '$domains', name = '$name', description = '$desc' where mid = $mentor_id";
+$sql = "UPDATE  mentordetails SET domain = '$domains', name = '$name', description = '$desc', choice2 = '$choice2', choice3 ='$choice3', choice4 = '$choice4', choice5= '$choice5', choice6 ='$choice6' where mid = $mentor_id";
 if(mysqli_query($con, $sql))
   {
     echo "Updated";
@@ -29,4 +26,14 @@ if(mysqli_query($con, $sql))
   }
 }
 
+ /*function getmentorpost($mentor_id)
+ {
+   $array = array();
+   $sql= "SELECT * FROM 'mentorpost' WHERE 'mid'=".$mentor_id;
+   while($row=mysqli_fetch_assoc($sql);)
+   {
+      $array['file_name'] = $row['file_name'];
+   }
+   return $array;}*/
+ 
 ?>
